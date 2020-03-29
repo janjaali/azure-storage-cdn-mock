2a6e119ec16805c9b757e295c90521d12fb2360a
Siyavash Habashi <ghashange@live.com>
2020-03-29 19:35:00 +0200

    Setup project (giter8 template: 'janjaali/scala.g8').

---
+++ app/src/main/scala/janjaali/AzureStorageCdnMock.scala
@@ -0,0 +1,13 @@
+package janjaali
+
+import com.typesafe.scalalogging.Logger
+
+object AzureStorageCdnMock {
+
+  private val logger = Logger("AzureStorageCdnMock")
+
+  def main(args: Array[String]): Unit = {
+
+    logger.info("azure-storage-cdn-mock started ...")
+  }
+}
---
+++ app/src/test/scala/janjaali/AzureStorageCdnMockSpec.scala
@@ -0,0 +1,14 @@
+package janjaali
+
+import org.scalatest.freespec._
+
+class AzureStorageCdnMockSpec extends AnyFreeSpec {
+
+  "azure-storage-cdn-mock" - {
+
+    "should succeed." in {
+
+      succeed
+    }
+  }
+}
